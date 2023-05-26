9b16045a824e58afc1031afa307f26744647ff46
nonuvrsthewrld <n.dias.lx@gmail.com>
2023-05-26 13:21:59 +0100

    criação charles.py

--- project_CIFO/Plots/Average Fitness x Generations.py
+++ project_CIFO/Plots/Average Fitness x Generations.py
@@ -33,21 +33,33 @@ def get_fitness(self):
 # Monkey Patching
 Individual.get_fitness = get_fitness
 
-def plot_fitness(generations, best_fitness_values):
-    plt.plot(generations, best_fitness_values)
+def plot_fitness(generations, avg_fitness_values, min_fitness_values, max_fitness_values):
+    # Plot average fitness values
+    plt.plot(generations, avg_fitness_values, label='Average', color='green')
+    # Plot max fitness values
+    plt.plot(generations, max_fitness_values, label='Max', color='grey')
+    # Plot min fitness values
+    plt.plot(generations, min_fitness_values, label='Min', color='grey')
+
+    # Fill the area between max and min with grey color
+    plt.fill_between(generations, min_fitness_values, max_fitness_values, color='grey', alpha=0.5)
+
     plt.xlabel('Generation')
     plt.ylabel('Fitness')
     plt.title('Average Fitness Variation')
+    plt.legend()
     plt.show()
 
 
 def evolution_process(num_runs=5):
-    average_fitness_values = []
+    avg_fitness_values = []
+    min_fitness_values = []
+    max_fitness_values = []
 
-    for _ in range(num_runs):
+    for generation in range(20, 100):
         best_fitness_values = []
 
-        for generation in range(20, 100):
+        for _ in range(num_runs):
             pop = Population(size=50, optim="min", sol_size=len(data_), valid_set=range(20), replacement=True)
             pop.evolve(gens=generation, xo_prob=0.9, mut_prob=0.2, select=tournament_sel,
                        mutate=swap_mutation, crossover=uniform_co, elitism=True)  # Evolve the population for 1 generation
@@ -59,15 +71,19 @@ def evolution_process(num_runs=5):
             # Store the best fitness value
             best_fitness_values.append(best_fitness)
 
-        average_fitness_values.append(best_fitness_values)
+        # Calculate average, min and max fitness values
+        avg_fitness_values.append(np.mean(best_fitness_values))
+        min_fitness_values.append(np.min(best_fitness_values))
+        max_fitness_values.append(np.max(best_fitness_values))
 
     # Calculate the average fitness values
     average_fitness_values = np.mean(average_fitness_values, axis=0)
 
     # Plot the fitness variation
     generations = np.arange(20, 100)
-    plot_fitness(generations, average_fitness_values)
+    plot_fitness(generations, avg_fitness_values, min_fitness_values, max_fitness_values)
 
 
 # Set up the population and other parameters
 evolution_process(num_runs=5)
+
