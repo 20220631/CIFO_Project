4b9e0a09a7039438caf1988fd36ea49aaab9b050
nonuvrsthewrld <n.dias.lx@gmail.com>
2023-05-27 11:54:49 +0100

    criação charles.py

--- project_CIFO/Plots/Average Fitness Per Combination.py
+++ project_CIFO/Plots/Average Fitness Per Combination.py
@@ -48,7 +48,7 @@ for mutate in mutation_methods:
             for _ in range(11):  # repeat the test N times
                 # Initialize the population
                 pop = Population(size=50, optim="min", sol_size=len(data_), valid_set=np.arange(0, 1.1, 0.01), replacement=True)
-
+                #ola
                 # Evolve the population
                 pop.evolve(gens=40, xo_prob=0.9, mut_prob=0.2, select=select, mutate=mutate, crossover=crossover,
                            elitism=True)
